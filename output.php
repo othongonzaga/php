442105189e8c9b1dcec87b2c42a4809356ac31e1
othongonzaga <othoncesar1@hotmail.com>
2022-03-03 10:36:42 -0300

    Aula sobre funções de saída

---
+++ output.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Aula sobre funções de saída
+ * 
+ * Funções a serem utilizadas: echo, var_dump
+ * Constantes a serem utilizadas: PHP_EOL
+ */
+
+echo "Olá". "-". 123 . PHP_EOL;
+
+var_dump(value:"dafafg");
+var_dump(value: true);
+var_dump(value: 12.2);
+var_dump(value: 12);
+var_dump(value: [1,2,3,4,5]);
