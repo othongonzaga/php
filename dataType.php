fb51613817ab22cfa5859a1a889d7b107423dcf9
othongonzaga <othoncesar1@hotmail.com>
2022-03-03 11:07:11 -0300

    Tipo de Dados

---
+++ dataType.php
@@ -0,0 +1,12 @@
+<?php
+// Aula sobre Tipos de Dados
+
+var_dump(value: 'eae, galera'); //string
+var_dump(value: true); //bool
+var_dump(value: 100); //int
+var_dump(value: 10.5); //double
+var_dump(['Othon', 20, 'Pernambuco']);// array
+
+//variavel começa com  $
+$dados = ['name' =>'Gabigol', 25, 'Flamengo'];
+var_dump($dados['name']);
