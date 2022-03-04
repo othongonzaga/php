fdcd227d7c9a36beffe2beb494a6920b63aecd98
othongonzaga <othoncesar1@hotmail.com>
2022-03-04 12:03:51 -0300

    Operadores Aritméticos e Operadores de Atribuição

---
+++ arithmeticsOperators.php
@@ -0,0 +1,28 @@
+<?php
+// Aula sobre Operadores Aritméticos
+
+$numA = 10;
+$numB = 20;
+$itemC = ($itemA + $itemB + 10) +10;
+
+echo $itemC . PHP_EOL;
+
+//divisão
+
+$itemC = $itemA / $itemB;
+
+echo $itemC . PHP_EOL;
+
+// multiplicação
+
+$itemA = 15;
+$itemB = 3;
+$itemC = ($itemA * $itemB) - 10;
+
+// Resto da divisão
+
+$item = 20;
+$itemB = 3;
+$itemC = $itemA % $itemB;
+
+echo $itemC . PHP_EOL;
---
+++ attributionsOperators.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Aula sobre Operadores de Atribuição
+ * 
+ * Operadores: +=  -=  /=  *=  %=
+ */
+
+ $itemA = 50;
+ $itemA += 25;
+
+ echo $itemA . PHP_EOL;
+
+ $itemA /= 2;
+
+ echo $itemA . PHP_EOL;
+
+ $itemA *= 3;
+
+ echo $itemA . PHP_EOL;
